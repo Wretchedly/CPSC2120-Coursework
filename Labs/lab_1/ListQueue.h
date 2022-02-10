d62443501b0c79e78ccf252030d6e84e842ec6dc
BuildTools <unconfigured@null.spigotmc.org>
2022-02-10 15:15:09 -0500

    Added lab 1

---
+++ Labs/lab_1/ListQueue.h
@@ -0,0 +1,90 @@
+/*
+ * Name: Chris Caldwell
+ * Date Submitted: 2/2/22
+ * Lab Section: 003
+ * Assignment Name: Lab1: Linked List Based Stacks and Queues
+ */
+
+#pragma once
+#include "List.h"
+
+//This class represents a Queue implemented via Linked List
+//Do not modify anything in the class interface
+template <class T>
+class ListQueue{
+
+ private:
+  List<T> queue;
+
+ public:
+  ListQueue();
+  ~ListQueue();
+  int size();
+  bool empty();
+  void enqueue(T);
+  T dequeue();
+  //Print the name and this ListQueue's size and values to stdout
+  //This function is already implemented (no need to change it)
+  void print(string name){
+    queue.print(name);
+  }
+
+}; //end of class interface (you may modify the code below)
+
+//Implement all of the functions below
+
+//Construct an empty ListQueue by initializing this ListQueue's instance variables
+template <class T>
+ListQueue<T>::ListQueue(){
+    queue = List<T>();
+}
+
+//Destroy all nodes in this ListQueue to prevent memory leaks
+template <class T>
+ListQueue<T>::~ListQueue(){
+    while(!empty()){
+        queue.removeStart();
+    }
+}
+
+//Return the size of this ListQueue
+template <class T>
+int ListQueue<T>::size(){
+    return queue.size();
+}
+
+//Return true if this ListQueue is empty
+//Otherwise, return false
+template <class T>
+bool ListQueue<T>::empty(){
+    bool isEmpty;
+    
+    if(queue.empty()){
+        return true;
+    }
+    
+    return false;
+}
+
+//Create a new node with value, and insert that new node
+//into this ListQueue in its correct position
+template <class T>
+void ListQueue<T>::enqueue(T value){
+    //if(queue.empty()){
+    //    queue.insertStart(value);
+    //} else {
+        queue.insertEnd(value);
+    //}
+}
+
+//Dequeue an element from the queue.
+//Note that here that means both removing it from the list
+//AND returning the value
+template <class T>
+T ListQueue<T>::dequeue(){
+    T nodeVal = queue.getFirst();
+    
+    queue.removeStart();
+    
+    return nodeVal;
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CPSC2120-Coursework
 Projects and lab assignments from CPSC2120 - Algorithms and Data Structures
+## CPSC2120 Course Description
+*Study of data structures and algorithms fundamental to computer science; abstract data-type concepts; measures of program running time and time complexity; algorithm analysis and design techniques.*
