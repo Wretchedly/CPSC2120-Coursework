a40e829d7383cdad7e50a6ece7e5a56eaae4ef6e
Wretchedly-Desktop <wretchedglitch@gmail.com>
2022-02-20 23:48:05 -0500

    Included the driver for testing

--- Labs/lab_4/Driver.cpp
+++ Labs/lab_4/Driver.cpp
@@ -1,5 +1,6 @@
 //For test purposes only, not submitting
 #include "Searches.h"
+#include "Sorts.h"
 #include "iostream"
 
 using namespace std;
@@ -12,6 +13,30 @@ int main(){
     cout<<"\nlinearSearch(intVec, -2) = "<<linearSearch(intVec, -2);
     cout<<"\nlinearSearch(charVec, e) = "<<linearSearch(charVec, 'e');
     cout<<"\nlinearSearch(charVec, B) = "<<linearSearch(charVec, 'B');
+
+    cout<<"\n\npre-sort:";
+    for(int i = 0; i < intVec.size(); i++){
+        cout<<intVec[i]<<" ";
+    }
+
+    intVec = quickSort(intVec);
+
+    cout<<"\n\npost-sort:";
+    for(int i = 0; i < intVec.size(); i++){
+        cout<<intVec[i]<<" ";
+    }
+
+    cout<<"\n\npre-sort:";
+    for(int i = 0; i < charVec.size(); i++){
+        cout<<charVec[i]<<" ";
+    }
+
+    charVec = quickSort(charVec);
+
+    cout<<"\n\npost-sort:";
+    for(int i = 0; i < charVec.size(); i++){
+        cout<<charVec[i]<<" ";
+    }
 
     return 0;
 }
